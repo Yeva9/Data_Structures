e3a1f997bcab6cbab594269c841313e23a0c6e59
Yeva <eva.hovnanyan.1999@gmail.com>
2021-03-30 12:28:22 +0400

    Added Vector Class Template.

--- classVector/Vector.cpp
+++ classVector/Vector.cpp
@@ -1,136 +1,321 @@
-#pragma <once>;
-#include "pch.h"
-#include <iostream>
+#include "Vector.hpp"
 
-class Vector
+template <typename T1>
+std::ostream& operator<<(std::ostream& out,const Vector<T1>& V)
 {
-public:
-	Vector();
-	Vector(const int* container, const int size);
-	~Vector();
-	Vector(const Vector& V);
-	int Get_Size() const { return m_size; }
-	bool Is_Empty() const { return m_size == 0; }
-	int Get_Element(int i) const { return m_container[i]; }
-	void Push_Back(int value);
-	void Pop_Back();
-	void Print() const;
-
-private:
-	int m_size;
-	int m_capacity;
-	int* m_container;
-};
-
-Vector::Vector()
-{
-	m_size = 0;
-	m_capacity = 1;
-	m_container = new int[m_size];
-}
-
-Vector::Vector(const int* container, const int size)
-	:m_size(size)
-	, m_capacity(1)
-	, m_container(new int[m_size])
-{
-	for (int i = 0; i < m_size; ++i)
-	{
-		m_container[i] = container[i];
+	if (!V.IsEmpty()) {
+		for(int i = 0; i < V.m_size; ++i) {
+			out << V.m_data[i] << ' ';
+		}
 	}
+	out << std::endl;
+	return out;
 }
 
-Vector::~Vector()
+template <typename T1>
+std::istream& operator>>(std::istream& in, Vector<T1>& V)
 {
-	delete[] m_container;
+	for(int i = 0; i < V.m_size; ++i) {
+		in >> V[i];
+	}
+	return in;
 }
 
-Vector::Vector(const Vector& V)
+template <typename T>
+Vector<T>::Vector()
+	:m_maxsize(MAXSIZE)
+	,m_size(0)
+	,m_data(nullptr)
 {
-	m_size = V.m_size;
-	m_capacity = V.m_capacity;
-	m_container = new int [m_size];
-	for (int i = 0; i < m_size; ++i)
-	{
-		m_container[i] = V.m_container[i];
+	AllocateAndInitialize();
+}
+
+template <typename T>
+Vector<T>::Vector(int size, const T* data)
+	:m_maxsize((size > MAXSIZE) ? size : MAXSIZE)
+	,m_size(size)
+	,m_data(nullptr)
+{
+	AllocateAndInitialize(data);
+}
+
+template <typename T>
+Vector<T>::Vector(const Vector<T>& V)
+	:m_maxsize(V.m_maxsize)
+	,m_size(V.m_size)
+	,m_data(nullptr) 
+{
+	AllocateAndInitialize(V.m_data);
+}
+
+template <typename T>
+Vector<T>::~Vector()
+{
+	Delocate();
+}
+
+template <typename T>
+Vector<T>& Vector<T>::operator=(const Vector<T>& V)
+{
+	if (this != &V) {
+		Delocate();
+		m_maxsize = V.m_maxsize;
+		m_size = V.m_size;
+		AllocateAndInitialize(V.m_data);
+	}
+	return *this;
+}
+
+template <typename T>
+T& Vector<T>::operator[](int index)
+{
+	CheckIndex(index);
+	return m_data[index];
+}
+
+template <typename T>
+const T& Vector<T>::operator[](int index) const
+{
+	CheckIndex(index);
+	return m_data[index];
+}
+
+template <typename T>
+void Vector<T>::PushBack(const T& element)
+{
+	if (m_size == m_maxsize) {
+		m_maxsize *= 2;
+
+		T* tmp = new T[m_maxsize];
+		
+		for (int i = 0; i < m_size; ++i) {
+			tmp[i] = m_data[i];
+		}
+
+		Delocate();
+		m_data = tmp;
+	}
+	m_data[m_size++] = element;
+}
+
+template <typename T>
+void Vector<T>::PopBack()
+{
+	if (! IsEmpty()) {
+		--m_size;
+		return;
+	}
+	std::cout << "Vector is empty:" << std::endl;
+	//throw VectorIsEmptyException();
+	return;
+}
+
+template <typename T>
+void Vector<T>::Insert(int index, const T& element)
+{
+	if (index < 0) {
+		std::cout << "Index is less than 0" << std::endl;
+		return;
 	}
 
+	if (index >= m_size) {
+		PushBack(element);
+	} else {
+		if (m_size == m_maxsize) {
+			m_maxsize *= 2;	
+		}
 
-}
+		T* tmp = new T[m_maxsize];
 
-void Vector::Push_Back(int value)
-{
-	if (m_size < m_capacity)
-	{
-		m_container[m_size] = value;
+		for (int i = 0; i < index; ++i) {
+			tmp[i] = m_data[i];
+		}
+
+		tmp[index] = element;
+
+		for (int i = index + 1; i <= m_size; ++i) {
+			tmp[i] = m_data[i - 1];
+		}
+
+		Delocate();
+		m_data = tmp;
 		++m_size;
 	}
-	else
-	{
-		int nCapacity = 2 * m_capacity;
-		int* nContainer = new int[nCapacity];
-		for (int i = 0; i < m_size; ++i)
-		{
-			nContainer[i] = m_container[i];
+}
+
+template <typename T>
+void Vector<T>::Remove(int index)
+{
+	if (index < 0) {
+		std::cout << "Index is less than 0" << std::endl;
+		return;
+	}
+
+	if (index >= m_size) {
+		PopBack();
+	} else {
+		T* tmp = new T[m_maxsize];
+
+		for (int i = 0; i < index; ++i) {
+			tmp[i] = m_data[i];
 		}
-		m_capacity = nCapacity;
-		int * tmp = m_container;
-		m_container = nContainer;
-		m_container[m_size] = value;
-		++m_size;
-		delete[] tmp;
+
+		for (int i = index; i < m_size - 1; ++i) {
+			tmp[i] = m_data[i + 1];
+		}
+		Delocate();
+		m_data = tmp;
+		--m_size;
+	}	
+}
+
+template <typename T>
+void Vector<T>::ShrinkToFit()
+{
+	if (0 == m_size) {
+		m_maxsize = m_size;
+		T* m_data = new T[m_maxsize];
+		return;
+	}
+
+	m_maxsize = m_size;
+	T* tmp = new T[m_maxsize];
+
+	for (int i = 0; i < m_size; ++i) {
+		tmp[i] = m_data[i];
+	}
+	Delocate();
+	m_data = tmp;
+}	
+
+template <typename T>
+void Vector<T>::Reserve(int nmaxsize)
+{
+	if (nmaxsize <= m_size) {
+		return;
+	}
+
+	if (0 == m_size) {
+		m_maxsize = nmaxsize;
+		T* m_data = new T[m_maxsize];
+		return;
+	}
+
+	m_maxsize = nmaxsize;
+	T* tmp = new T[nmaxsize];
+
+	for (int i = 0; i < m_size; ++i) {
+		tmp[i] = m_data[i];
+	}
+	Delocate();
+	m_data = tmp;
+}	
+	
+template <typename T>
+int Vector<T>::GetSize() const
+{
+	return m_size;
+}
+
+template <typename T>
+int Vector<T>::GetCapacity() const
+{
+	return m_maxsize;
+}
+
+template <typename T>
+bool Vector<T>::IsEmpty() const
+{
+	return m_size == 0;
+}
+
+template <typename T>
+void Vector<T>::AllocateAndInitialize(const T* data)
+{
+	m_data = new T[m_maxsize] {};
+
+	if (nullptr != data) {
+		assert(nullptr != data);
+
+		for (int i = 0; i < m_size; ++i) {
+			m_data[i] = data[i];
+		}
+		assert(nullptr != data);
 	}
 }
 
-void Vector::Pop_Back()
+template <typename T>
+void Vector<T>::Delocate()
 {
-	if (Is_Empty()) return;
-	if (m_size * 4 > m_capacity)
-	{
-		--m_size;
-	}
-	else
-	{
-		int nCapacity = m_capacity / 2;
-		int* nContainer = new int[nCapacity];
-		for (int i = 0; i < m_size; ++i)
-		{
-			nContainer[i] = m_container[i];
-		}
-		m_capacity = nCapacity;
-		int * tmp = m_container;
-		m_container = nContainer;
-		--m_size;
-		delete[] tmp;
-	}
+	delete[] m_data;
+	m_data = nullptr;
 }
 
-void Vector::Print() const
+template <typename T>
+void Vector<T>::CheckIndex(int index)
 {
-	for (int i = 0; i < m_size; ++i)
-	{
-		std::cout << m_container[i] << " ";
-	}
-	std::cout << std::endl;
+	assert(index > 0 || index < m_size);
+
+	if (index < 0 || index >= m_size) {
+		std::cout << "Index is out of bounds" << std::endl;
+		return;
+		}
+	//ok
 }
 
 int main()
 {
-	const int n = 10;
-	int A[n] = { 0,1,2,3,4,5,6,7,8,9 };
+	/***************testing**************/
+	Vector<int> V;
 
-	Vector V1(A, n);
-	/*V1.Pop_Back();
-	V1.Push_Back(20);
-	V1.Push_Back(20);
-	V1.Print();
+	V.PushBack(1);
+	V.PushBack(2);
+	V.PushBack(3);
+	V.PushBack(4);
+	V.PushBack(5);
+	V.PushBack(6);
+	V.PushBack(7);
+	V.PopBack();
 
+	std::cout << V << std::endl;
 
-	Vector V2;
-	V2.Push_Back(10);
-	V2.Push_Back(10);
-	V2.Print();*/
-	Vector V3 = V1;
-	V3.Print();
+	std::cout << "Size is: " << V.GetSize() << std::endl;
+	std::cout << "Capacity is: " << V.GetCapacity() << std::endl;
+	std::cout << "Is empty? " << V.IsEmpty() << std::endl;
+	V.Insert(2,100);
+	V.Remove(4);
+
+	std::cout << V << std::endl;
+	std::cout << "************************************************" << std::endl;
+	
+	const int n = 200;
+	int arr[n] = {0};
+
+	Vector<int> V2(n,arr);
+	for (int i = 0; i < V2.GetSize(); ++i) {
+      		V2[i] = i;
+    }
+
+	std::cout << "Size is: " << V2.GetSize() << std::endl;
+	std::cout << "Capacity is: " << V2.GetCapacity() << std::endl;
+	std::cout <<"***************************************************"<<std::endl;
+
+	Vector<int> V3;
+
+	V3.PushBack(1);
+	V3.PushBack(2);
+	V3.PushBack(4);
+
+	std::cout << "Size is: " << V3.GetSize() << std::endl;
+	std::cout << "Capacity is: " << V3.GetCapacity() << std::endl;
+	
+	V3.Reserve(4);
+	std::cout << "Size after reserve: " << V3.GetSize() << std::endl;
+	std::cout << "Capacity after reserve: " << V3.GetCapacity() << std::endl;
+	
+	V3.ShrinkToFit();
+	std::cout << "Size after shrink: " << V3.GetSize() << std::endl;
+	std::cout << "Capacity after shrink: " << V3.GetCapacity() << std::endl;
+	
+	return 0;
 }
-
